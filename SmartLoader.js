5e84e791c3ee1809d8be240b968aced9d5ce98ab
Gentille Plume <melvin.lemoine@feather-project.org>
2018-09-18 09:28:41 +0200

    Developed the script (work in progress)

---
+++ SmartLoader.js
@@ -0,0 +1,46 @@
+/*
+##################################################
+                    CONFIG
+##################################################
+*/
+
+var loader_image_path = "";
+
+/*
+##################################################
+                    ENGINE
+##################################################
+*/
+
+var overlay = document.createElement("div");
+overlay.setAttribute("id", "slOverlay");
+$("body")[0].appendChild(overlay);
+
+var loader = document.createElement("img");
+loader.setAttribute("id", "slLoader");
+loader.setAttribute("src", "../loaders/gears.svg");
+overlay.appendChild(loader);
+
+
+overlay.style.display = "flex";
+overlay.style.position = "absolute";
+overlay.style.width = "100vw";
+overlay.style.height = "100vh";
+overlay.style.justifyContent = "center";
+overlay.style.alignItems = "center";
+
+overlay.style.backgroundColor = "white";
+overlay.style.transition = "opacity 2s ease-in-out 0s";
+loader.style.transition = "opacity 1s ease-in-out 0s";
+
+
+
+$('*').waitForImages().done(function () {
+    overlay.style.opacity = 0;
+    loader.style.opacity = 0;
+
+    setTimeout(function () {
+        overlay.style.display = "none";
+        loader.style.display = "none";
+    }, 2000);
+});
