ee94f78feac0974e0633cc9927baf0101315b22d
Gentille Plume <melvin.lemoine@feather-project.org>
2018-09-18 20:02:14 +0200

    Improved the script

--- AmethystLoader.js
+++ AmethystLoader.js
@@ -5,14 +5,15 @@
 */
 
 // Loader path
-var loader_image_path = "";
+var loaders_path = "../loaders/";
+var loader_image = "energy";
 
 // Message to display after the content loader
 var loader_text = false;
 var loader_text_content = "";
 
 // Loader hide animation duration after all content loaded (default 2s)
-var loader_duration = 2;
+var loader_duration = 1;
 
 /*
 ##################################################
@@ -26,20 +27,20 @@ $("body")[0].appendChild(overlay);
 
 var loader = document.createElement("img");
 loader.setAttribute("id", "slLoader");
-loader.setAttribute("src", "../loaders/gears.svg");
-loader.setAttribute("class", "animated jackInTheBox");
-overlay.appendChild(loader);
+loader.setAttribute("src", loaders_path + loader_image + ".svg");
+//loader.setAttribute("class", "animated jackInTheBox");
+$("body")[0].appendChild(loader);
 
-overlay.style.transition = "opacity " + loader_duration + "s" + " ease-in-out 2s";
-loader.style.transition = "opacity " + loader_duration / 2 + "s" + " ease-in-out 1s";
+overlay.style.transition = "opacity " + loader_duration + "s" + " ease-in-out 0s";
+loader.style.transition = "opacity " + loader_duration / 2 + "s" + " ease-in-out 0s";
 
 $('*').waitForImages().done(function () {
-        overlay.style.opacity = 0;
-        loader.style.opacity = 0;
 
-        setTimeout(function () {
-            overlay.style.display = "none";
-            loader.style.display = "none";
-        }, loader_duration * 200000);
+    overlay.style.opacity = 0;
+    loader.style.opacity = 0;
 
+    setTimeout(function () {
+        overlay.style.display = "none";
+        loader.style.display = "none";
+    }, loader_duration * 1000);
 });
