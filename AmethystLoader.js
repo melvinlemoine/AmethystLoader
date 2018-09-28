4c34d8453d634ba6669c86f6d8ec40fb32640d0e
GentillePlume <melvin.lemoine@feather-project.org>
2018-09-28 09:51:59 +0200

    Removed useless things and improved the script

--- AmethystLoader.js
+++ AmethystLoader.js
@@ -1,43 +1,25 @@
-/*
-##################################################
-                    CONFIG
-##################################################
-*/
+function AmethystLoader(loaders_folder, loader_image) {
+  var overlay = document.createElement("div");
+  overlay.setAttribute("id", "slOverlay");
+  $("body")[0].appendChild(overlay);
 
-// Loader path
-var loaders_path = "AmethystLoader/loaders/";
-var loader_image = "gears.svg";
+  var loader = document.createElement("img");
+  loader.setAttribute("id", "slLoader");
+  loader.setAttribute("src", loaders_folder + loader_image);
+  //loader.setAttribute("class", "animated jackInTheBox");
+  $("body")[0].appendChild(loader);
 
-// Message to display after the content loader
-var loader_text = false;
-var loader_text_content = "";
+  overlay.style.transition = "opacity " + 1 + "s" + " ease-in-out 0s";
+  loader.style.transition = "opacity " + 1 / 2 + "s" + " ease-in-out 0s";
 
-/*
-##################################################
-                    ENGINE
-##################################################
-*/
+  $('*').waitForImages(true).done(function() {
 
-var overlay = document.createElement("div");
-overlay.setAttribute("id", "slOverlay");
-$("body")[0].appendChild(overlay);
+    overlay.style.opacity = 0;
+    loader.style.opacity = 0;
 
-var loader = document.createElement("img");
-loader.setAttribute("id", "slLoader");
-loader.setAttribute("src", loaders_path + loader_image);
-//loader.setAttribute("class", "animated jackInTheBox");
-$("body")[0].appendChild(loader);
-
-overlay.style.transition = "opacity " + 1 + "s" + " ease-in-out 0s";
-loader.style.transition = "opacity " + 1 / 2 + "s" + " ease-in-out 0s";
-
-$('*').waitForImages(true).done(function() {
-
-  overlay.style.opacity = 0;
-  loader.style.opacity = 0;
-
-  setTimeout(function() {
-    overlay.style.display = "none";
-    loader.style.display = "none";
-  }, 1000);
-});
+    setTimeout(function() {
+      overlay.style.display = "none";
+      loader.style.display = "none";
+    }, 1000);
+  });
+}
